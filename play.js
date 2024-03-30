eefb2e73044e54bc5272239b8aa67689354d1c96
unknown <jamieohakwe@gmail.com>
2024-03-30 06:16:38 +0100

    landscape card

--- play.js
+++ play.js
@@ -8,11 +8,38 @@
 
 
 
-document.getElementById('nameButton').onclick = function() {
-    // console.log('The button works!');
+// document.getElementById('nameButton').onclick = function(){
+//     // console.log('The button works!');
 
-   let nameBox =  document.getElementById('nameBox').value 
-   document.getElementById('myName').innerHTML = nameBox
-   document.getElementById('myName').style.color = 'blue'
-   console.log(nameBox);
+//    let nameBox =  document.getElementById('nameBox').value 
+//    document.getElementById('myName').innerHTML = nameBox
+//    document.getElementById('myName').style.color = 'blue'
+//    console.log(nameBox);
+// }
+
+
+
+function testWorking(){
+    // console.log('I am working!');
+    let value1 = document.getElementById('input1').value
+    let value2 = document.getElementById('input2').value
+    let value3 = document.getElementById('input3').value
+
+   
+    document.getElementById('answer1').innerHTML = value1
+    document.getElementById('answer2').innerHTML = value2
+    document.getElementById('answer3').innerHTML = value3
+    document.getElementById('answer1').style.color = 'purple'
+    document.getElementById('answer2').style.color = 'purple'
+    document.getElementById('answer3').style.color = 'purple'
+   
 }
+
+// testWorking()
+
+
+
+
+// sayHello('Jamie', 'Hello')
+// let x = 12 ley y = 7 
+// turnOnLight()   openDoor()   changeColor()
